12d0d6be2e714753a7fdb813fadb88aadf0f7253
Myx-x <mkamadeus.mka@gmail.com>
2019-08-09 01:21:14 +0700

    Navigation added woohoo o/; Added some login page changes

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,3 +1,11 @@
 function toggleSection(sectionName){
     
+    var x = document.getElementsByTagName("section");
+    for (let i = 0; i < x.length; i++) {
+        const element = x[i];
+        if(element.className!=sectionName)element.style.display = "none";
+    }
+    
+    var section = document.getElementById(sectionName);
+    section.style.display = "block";
 }
